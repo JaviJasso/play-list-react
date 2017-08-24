aa8cfe71dadd93ddc8a99b057691260d798d4f3c
Javi <jjasso1@asu.edu>
2017-08-24 09:16:12 -0400

    events, setting setState

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,18 +1,33 @@
 import React, { Component} from 'react';
 
 class Form extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      artist: '',
+      title: '',
+      notes: '',
+    };
+  }
+  
+  onChangeHandler = (key, value) => {
+    this.setState({[key]: value});
+  }
 
   render(){
     return (
       <form>
-        <input type="text" onChange={} value={username}></input>
-        <input type="text" onChange={} value={artist}></input>
-        <input type="text" onChange={} value={title}></input>
-        <textarea onChange={} >
-          {notes}
+        <input type="text" onChange={(event)} value={this.state.username}></input>
+        <input type="text" onChange={(event)} value={this.state.artist}></input>
+        <input type="text" onChange={(event)} value={this.state.title}></input>
+        <textarea onChange={(event) => this.onChangeHandler('notes', event.target.value)}>
+          {this.state.notes}
         </textarea>
         <button type="submit">ADD SONG!</button>
       </form>
     )
   }
 }
+
+export default Form
